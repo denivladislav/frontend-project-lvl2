7574caaa56243f1c5d12ec200a3d181353badc78
vvvhatislove <vladenisov1234@yandex.ru>
2021-01-18 21:40:58 +0300

    arguments and oprion added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,9 @@ import commander from 'commander';
 const program = commander.program;
 
 program
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows the difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
 
 program.parse();
