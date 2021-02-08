c70b162ebf31a80e8f83aeebb0ba1ecc0607265e
vvvhatislove <vladenisov1234@yandex.ru>
2021-02-08 18:49:38 +0300

    parse  is set as a standalone function

---
+++ src/parsers.js
@@ -0,0 +1,18 @@
+import path from 'path';
+import fs from 'fs';
+import yaml from 'js-yaml';
+
+export default (filepath) => {
+  const extname = path.extname(filepath).slice(1).trim();
+  const data = fs.readFileSync(path.resolve(filepath));
+  switch (extname) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+      return yaml.load(data);
+    case 'yaml':
+      return yaml.load(data);
+    default:
+      throw new Error('Unsupported format');
+  }
+};
