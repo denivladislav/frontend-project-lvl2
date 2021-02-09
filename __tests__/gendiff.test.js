c744a75eb49f19511c6453b0e16ba5825ea7c022
vvvhatislove <vladenisov1234@yandex.ru>
2021-02-09 20:00:14 +0300

    tree tests added

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,15 +11,20 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 const resultJSON = readFile('resultJSON.diff');
 const resultyml = readFile('resultyml.diff');
+const resulttree = readFile('resulttree.diff');
 
-const filepath1json = getFixturePath('file1plain.json');
-const filepath2json = getFixturePath('file2plain.json');
+const plain1jsonpath = getFixturePath('plain1.json');
+const plain2jsonpath = getFixturePath('plain2.json');
 
-const filepath1yml = getFixturePath('file1plain.yml');
-const filepath2yml = getFixturePath('file2plain.yml');
+const plain1ymlpath = getFixturePath('plain1.yml');
+const plain2ymlpath = getFixturePath('plain2.yml');
+
+const tree1jsonpath = getFixturePath('tree1.json');
+const tree2jsonpath = getFixturePath('tree2.json');
 
 test('plain', () => {
-  expect(genDiff(filepath1json, filepath2json)).toEqual(resultJSON);
-  expect(genDiff(filepath1yml, filepath2yml)).toEqual(resultyml);
-  expect(genDiff(filepath1yml, filepath2json)).toEqual(resultJSON);
+  expect(genDiff(plain1jsonpath, plain2jsonpath)).toEqual(resultJSON);
+  expect(genDiff(plain1ymlpath, plain2ymlpath)).toEqual(resultyml);
+  expect(genDiff(plain1ymlpath, plain2jsonpath)).toEqual(resultJSON);
+  expect(genDiff(tree1jsonpath, tree2jsonpath)).toEqual(resulttree);
 });
