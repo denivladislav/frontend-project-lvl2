ba811cae118ea8ec6e649f3e76051cec2955ab0c
vvvhatislove <vladenisov1234@yandex.ru>
2021-02-11 23:32:19 +0300

    buildTree.js is renamed; map instead of reduce and new things returned; stringify changed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-// import _ from 'lodash';
-import stringify from '../utils/stringify.js';
-import buildTree from '../utils/buildTree.js';
+import buildDiffTree from '../utils/buildDiffTree.js';
+import makeStylish from '../utils/newStringify.js';
 import parse from './parsers.js';
 
 const genDiff = (filepath1, filepath2) => {
   const parsedData1 = parse(filepath1);
   const parsedData2 = parse(filepath2);
 
-  const finalResult = (buildTree(parsedData1, parsedData2));
-  console.log(finalResult);
-  console.log(stringify(finalResult));
-  return stringify(finalResult);
+  const finalResult = (buildDiffTree(parsedData1, parsedData2));
+  console.log(makeStylish(finalResult));
+  return makeStylish(finalResult);
 };
 
 export default genDiff;
