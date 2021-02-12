522e32e15f82780c04f522f37e8d1b8117b12db7
vvvhatislove <vladenisov1234@yandex.ru>
2021-02-12 18:19:29 +0300

    mistake fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import buildDiffTree from '../utils/buildDiffTree.js';
 import parse from './parsers.js';
 import formatise from './formatters/index.js';
 
-const genDiff = (filepath1, filepath2, format) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const parsedData1 = parse(filepath1);
   const parsedData2 = parse(filepath2);
   const finalResult = (buildDiffTree(parsedData1, parsedData2));
