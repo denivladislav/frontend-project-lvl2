cecebc3d0a3f34548f4b3f01fed28f1ec078d5d7
vvvhatislove <vladenisov1234@yandex.ru>
2021-02-14 14:48:24 +0300

    plain formatter added

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,12 @@
 import makeStylish from './stylish.js';
+import makePlain from './plain.js';
 
 export default (data, format) => {
   switch (format) {
     case 'stylish':
       return makeStylish(data);
+    case 'plain':
+      return makePlain(data);
     default:
       throw new Error(`Unsupported output format: ${format}`);
   }
---
+++ src/formatters/plain.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+
+const stringify = (currentValue) => {
+  if (currentValue === null) {
+    return 'null';
+  }
+  if (_.isBoolean(currentValue)) {
+    return currentValue;
+  }
+  if (!_.isObject(currentValue)) {
+    return `'${currentValue.toString()}'`;
+  }
+  return '[complex value]';
+};
+
+const makePlainTree = (tree) => {
+  const iter = (nodes, pathToRoot = '') => (
+    nodes.flatMap((node) => {
+      const {
+        key, value, status, oldValue, newValue, children,
+      } = node;
+      switch (status) {
+        case 'deleted':
+          return `Property '${pathToRoot}${key}' was removed`;
+        case 'added':
+          return `Property '${pathToRoot}${key}' was added with value: ${stringify(value)}`;
+        case 'modified':
+          return `Property '${pathToRoot}${key}' was updated. From: ${stringify(oldValue)} to ${stringify(newValue)}`;
+        case 'unmodified':
+          return '';
+        case 'nested':
+          return [...iter(children, `${pathToRoot}${key}.`)];
+        default:
+          throw new Error(`Unknown status: ${status}`);
+      }
+    })
+  );
+  const lines = iter(tree);
+  return [...lines.filter((line) => line)].join('\n');
+};
+
+export default makePlainTree;
