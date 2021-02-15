1fb580c830bdcdcd68206d793a84c6ea262311ef
vvvhatislove <vladenisov1234@yandex.ru>
2021-02-16 01:22:02 +0300

    .sort() changed to lodash function _.sortBy()

--- utils/buildDiffTree.js
+++ utils/buildDiffTree.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 const buildDiffTree = (data1, data2) => {
   const uniqKeys = new Set([...Object.keys(data1), ...Object.keys(data2)]);
-  const sortedKeys = Array.from(uniqKeys).sort();
+  const sortedKeys = _.sortBy(Array.from(uniqKeys));
   return sortedKeys.map((key) => {
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
       return { key, status: 'nested', children: buildDiffTree(data1[key], data2[key]) };
