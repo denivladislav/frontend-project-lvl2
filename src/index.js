4b2ef2bcb9882d71efdd2d16805a579e13ce56a0
vvvhatislove <vladenisov1234@yandex.ru>
2021-02-22 18:54:04 +0300

    fixes added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import buildDiffTree from '../utils/buildDiffTree.js';
+import path from 'path';
+import fs from 'fs';
+import buildDiffTree from './buildDiffTree.js';
 import parse from './parsers.js';
 import formatise from './formatters/index.js';
 
+const getData = (filepath) => ({
+  content: fs.readFileSync(path.resolve(filepath)),
+  type: path.extname(filepath).slice(1).trim(),
+});
+
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const parsedData1 = parse(filepath1);
-  const parsedData2 = parse(filepath2);
-  const finalResult = (buildDiffTree(parsedData1, parsedData2));
+  const data1 = getData(filepath1);
+  const data2 = getData(filepath2);
+  const parsedData1 = parse(data1);
+  const parsedData2 = parse(data2);
+  const finalResult = buildDiffTree(parsedData1, parsedData2);
   return formatise(finalResult, format);
 };
 
